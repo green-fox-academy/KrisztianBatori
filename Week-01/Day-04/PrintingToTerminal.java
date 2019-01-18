613b750f8d8d023f72df5193a5d01b97d36a4e14
KrisztianBatori <batori.krisztian97@gmail.com>
2019-01-18 08:59:58 +0100

    Java workshop exercise - 'Printing to terminal'

---
+++ Week-01/Day-04/PrintingToTerminal.java
@@ -0,0 +1,17 @@
+public class PrintingToTerminal {
+    public static void main(String[] args) {
+        // 01 - HelloMe
+        System.out.println("Hello, Batori!");
+
+        // 02 - HumptyDumpty
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("All the king's horses and all the king's men");
+        System.out.println("Couldn't put Humpty together again.");
+
+        // 03 - HelloOthers
+        System.out.println("Hello Jane!");
+        System.out.println("Hello Jones!");
+        System.out.println("Hello Jack!");
+    }
+}
