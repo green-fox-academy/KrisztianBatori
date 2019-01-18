874bda0d48bf1270239ac4d6f214c3d1d59a6b35
KrisztianBatori <batori.krisztian97@gmail.com>
2019-01-18 09:13:03 +0100

    Java workshop exercise - 'Types'

---
+++ Week-01/Day-04/Types.java
@@ -0,0 +1,22 @@
+public class Types {
+    public static void main(String[] args) {
+        // 04 - Introduce Yourself
+        System.out.println("Batori Krisztian");
+        System.out.println(21);
+        System.out.println(1.72);
+
+        // 05 - Two Numbers
+        int a = 22;
+        int b = 13;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(22. / 13);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // 06 - Coding Hours
+        System.out.println(6*5*17);
+        System.out.println(6.*5.*17./(52*17/100));
+    }
+}
