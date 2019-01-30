24443807e585fc27279317801bf7fa6bab388fb9
KrisztianBatori <batori.krisztian97@gmail.com>
2019-01-30 20:13:54 +0100

    'Classes and objects' Java workshop - Fleet of Things

---
+++ Week-03/Day-03/classWorkshop/FleetOfThings.java
@@ -0,0 +1,19 @@
+package classWorkshop;
+
+public class FleetOfThings {
+    public static void main(String[] args) {
+        Fleet fleet = new Fleet();
+        // Create a fleet of things to have this output:
+        // 1. [ ] Get milk
+        // 2. [ ] Remove the obstacles
+        // 3. [x] Stand up
+        // 4. [x] Eat lunch
+
+        fleet.add(new Thing("Get milk"));
+        fleet.add(new Thing("Remove the obstacles"));
+        fleet.add(new Thing("Stand up") {{ this.complete(); }} );
+        fleet.add(new Thing("Eat lunch") {{ this.complete(); }} );
+
+        System.out.println(fleet);
+    }
+}
--- Week-03/Day-03/classWorkshop/Main.java
+++ Week-03/Day-03/classWorkshop/Main.java
@@ -9,28 +9,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        // 06 - Pokemon
-        List<Pokemon> pokemonOfAsh = initializePokemons();
-
-        Pokemon wildPokemon = new Pokemon("Oddish", "leaf", "water");
-
-        for (Pokemon pokemon: pokemonOfAsh) {
-            if (pokemon.isEffectiveAgainst(wildPokemon)) {
-                System.out.print("I choose you, " + pokemon.name + "!");
-                System.out.println();
-                break;
-            }
-        }
-
-    private static List<Pokemon> initializePokemons() {
-        List<Pokemon> pokemon = new ArrayList<>();
-
-        pokemon.add(new Pokemon("Balbasaur", "leaf", "water"));
-        pokemon.add(new Pokemon("Pikatchu", "electric", "water"));
-        pokemon.add(new Pokemon("Charizard", "fire", "leaf"));
-        pokemon.add(new Pokemon("Balbasaur", "water", "fire"));
-        pokemon.add(new Pokemon("Kingler", "water", "fire"));
-
-        return pokemon;
+        // 07 - Fleet of things
+        // main() is at FleetOfThings
     }
 }
