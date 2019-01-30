2dc0403dc67cb2f465244e2ce72cf221d4a13651
KrisztianBatori <batori.krisztian97@gmail.com>
2019-01-30 20:15:22 +0100

    'Classes and objects' Java workshop - Dice set

--- Week-03/Day-03/classWorkshop/Main.java
+++ Week-03/Day-03/classWorkshop/Main.java
@@ -9,7 +9,25 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        // 07 - Fleet of things
-        // main() is at FleetOfThings
+        // 08 - Dice set
+        DiceSet diceSet = new DiceSet();
+        diceSet.roll();
+
+        while (!diceSet.getCurrent().equals(new ArrayList<Integer>() {{ for (int i = 0; i < 6; i++) { add(6); } }}))
+        {
+            if (!diceSet.getCurrent().contains(6)) {
+                diceSet.reroll();
+            }
+            else {
+                for (int i = 0; i < diceSet.getCurrent().size(); i++) {
+                    if (diceSet.getCurrent(i) != 6) {
+                        diceSet.reroll(i);
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(diceSet.getCurrent());
+
     }
 }
