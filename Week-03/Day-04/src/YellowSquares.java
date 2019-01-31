6a10ddece4954845311b928d3480c68c2fcce480
KrisztianBatori <batori.krisztian97@gmail.com>
2019-01-31 19:23:17 +0100

    'Recursion' - Java workshop (Square drawing exercise)

---
+++ Week-03/Day-04/src/YellowSquares.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class YellowSquares {
+    public static void mainDraw(Graphics graphics) {
+
+        drawSquares(graphics, 0, 0, 486, 486);
+
+        /*graphics.drawRect(241, 0, 2, 2);
+        graphics.drawRect(241, 4, 2, 2);
+        graphics.drawRect(239, 2, 2,2);
+        graphics.drawRect(243, 2, 2, 2);
+
+
+        graphics.drawRect(162, 0, 162, 162);
+        graphics.drawRect(162, 324, 162, 162);
+        graphics.drawRect(0, 162, 162,162);
+        graphics.drawRect(324, 162, 162, 162);*/
+    }
+    public static void drawSquares (Graphics graphics, int x, int y, int width, int height) {
+        if (width == 2 && height == 2) {
+            drawSquare(graphics, x, y, width, height);
+        }
+        else {
+            drawSquare(graphics, x, y, width, height);
+            drawSquares(graphics, x + width / 3, y, width / 3, height / 3);
+            drawSquares(graphics, x + width / 3, y + height * 2, width / 3, height / 3);
+            drawSquares(graphics, x - (width / 3) * 2, y + height, width / 3, height / 3);
+            drawSquares(graphics, x + ((width / 3) * 2) * 2, y + height, width / 3, height / 3);
+        }
+    }
+
+    public static void drawSquare(Graphics graphics, int x, int y, int width, int height) {
+        graphics.drawRect(x, y, width, height);
+        graphics.drawRect(x, y + height * 2, width, height);
+        graphics.drawRect(x - width, y + height, width, height);
+        graphics.drawRect(x + width, y + height, width, height);
+    }
+
+    static int WIDTH = 486;
+    static int HEIGHT = 486;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            super.setBackground(Color.YELLOW);
+            mainDraw(graphics);
+        }
+    }
+}
