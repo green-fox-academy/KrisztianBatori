148bff9d41eb577f28a08a306e00929bc3160dda
KrisztianBatori <batori.krisztian97@gmail.com>
2019-01-31 11:18:50 +0100

    'Recursion' - Java workshop (String2)

---
+++ Week-03/Day-04/src/String2.java
@@ -0,0 +1,13 @@
+public class String2 {
+    public static void main(String[] args) {
+        System.out.println(removeX("xxxxxxxx3345lkfdgklbxjhxabhxauinauxneoaxoianxnxnawiuwioyyyawoyxuxa"));
+    }
+    public static String removeX (String text) {
+        if (text.contains("x")) {
+            return removeX(text.replace("x", ""));
+        }
+        else {
+            return text;
+        }
+    }
+}
