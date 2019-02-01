f501cdc2ad85dfaeda9c2e408abbe401a4a88dbb
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-01 16:58:46 +0100

    'Inheritance' Java workshop - Green fox organization exercise -> Created Mentor class

--- Week-04/src/GreenFoxOrganization.java
+++ Week-04/src/GreenFoxOrganization.java
@@ -58,6 +58,31 @@ public class GreenFoxOrganization {
         }
     }
 
+    class Mentor extends Person {
+        String level;
+
+        Mentor() {
+            super();
+            level = "intermediate";
+        }
+
+        Mentor(String name, int age, String gender, String level) {
+            super(name, age, gender);
+            this.level = level;
+        }
+
+        @Override
+        public void getGoal() {
+            System.out.println("Educate brilliant junior software developers.");
+        }
+
+        @Override
+        public void introduce() {
+            super.introduce();
+            System.out.println(level + " mentor.");
+        }
+    }
+
     public static void main(String[] args) {
 
 
