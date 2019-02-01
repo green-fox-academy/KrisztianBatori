4b36ecb2d511e2157a8388d51639b9a0f3c96d16
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-01 12:02:26 +0100

    'Fractals' Java workshop - Circles (complete)

---
+++ Week-03/Day-04/src/Circles.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Circles {
+
+    static Color[] rainbowColors = new Color[] {Color.RED, Color.ORANGE, Color.BLACK,
+            Color.MAGENTA, Color.BLUE};
+
+    public static void mainDraw(Graphics graphics) {
+
+        drawCircles(graphics, 0, 0, WIDTH, HEIGHT, 6, 0);
+
+    }
+    public static void drawCircles (Graphics graphics, int x, int y, int width, int height, int size, int colorIndex) {
+        if (size != 1) {
+            drawCircle(graphics, x, y, width, height, colorIndex);
+            drawCircles(graphics, x + width / 4, y, width / 2, height / 2, size - 1, colorIndex + 1);
+            drawCircles(graphics, x + width / 58, y + height / 3, width / 2, height / 2, size - 1, colorIndex + 1);
+            drawCircles(graphics, x + width / 2 - width / 58, y + height / 3, width / 2, height / 2, size - 1, colorIndex + 1);
+        }
+    }
+
+    public static void drawCircle(Graphics graphics, int x, int y, int width, int height, int colorIndex) {
+        graphics.setColor(rainbowColors[colorIndex]);
+        graphics.drawOval(x, y, width, height);
+    }
+
+    static int WIDTH = 486;
+    static int HEIGHT = 486;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            super.setBackground(Color.WHITE);
+            mainDraw(graphics);
+        }
+    }
+}
