97c379db6c5c4c5c5dc12fda97b2d29238540be2
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-05 17:47:38 +0100

    'Garden App' - Java workshop --- MODIFIED Garden class, it has an output method, functionality can be tested

--- Week-04/src/thegardenapplication/Garden.java
+++ Week-04/src/thegardenapplication/Garden.java
@@ -5,7 +5,24 @@ import java.util.ArrayList;
 public class Garden {
     ArrayList<Plant> plants;
 
+    public void getNeedyPlants() {
+        for (int i = 0; i < plants.size(); i++) {
+            String plantType = plants.get(i).type;
+            if (plantType.equals("Flower") && plants.get(i).waterAmount < 5) {
+                System.out.println("The " + plants.get(i).color + " Flower needs water");
+            }
+        }
+
+        for (int i = 0; i < plants.size(); i++) {
+            String plantType = plants.get(i).type;
+            if (plantType.equals("Tree") && plants.get(i).waterAmount < 10) {
+                System.out.println("The " + plants.get(i).color + " Tree needs water");
+            }
+        }
+    }
+
     public void waterPlants(int waterAmount) {
+        System.out.println("Watering with " + waterAmount);
         int plantsNeededWaterCount = (int)plants.stream()
                                         .filter(plant -> plant.type.equals("Tree") && plant.waterAmount < 10)
                                         .count();
