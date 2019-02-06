4923f0f3a11a96941e07ddf696ee1848cac02663
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-06 12:04:28 +0100

    'Comparable' - Java workshop --- DONE Comparable exercise

---
+++ Week-04/src/comparable/Domino.java
@@ -0,0 +1,30 @@
+package comparable;
+import java.lang.*;
+
+class Domino implements Comparable<Domino> {
+    private final int left;
+    private final int right;
+
+    public Domino(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public int getLeftSide() {
+        return left;
+    }
+
+    public int getRightSide() {
+        return right;
+    }
+
+    @Override
+    public int compareTo(Domino o) {
+        return toString().compareTo(o.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "[" + left + ", " + right + "]";
+    }
+}
---
+++ Week-04/src/comparable/Dominoes.java
@@ -0,0 +1,26 @@
+package comparable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Dominoes {
+    public static void main(String[] args) {
+        List<Domino> dominoes = initializeDominoes();
+
+        Collections.sort(dominoes);
+
+        System.out.println(dominoes);
+    }
+
+    static List<Domino> initializeDominoes() {
+        List<Domino> dominoes = new ArrayList<>();
+        dominoes.add(new Domino(5, 2));
+        dominoes.add(new Domino(4, 6));
+        dominoes.add(new Domino(1, 5));
+        dominoes.add(new Domino(6, 7));
+        dominoes.add(new Domino(2, 4));
+        dominoes.add(new Domino(7, 1));
+        return dominoes;
+    }
+}
