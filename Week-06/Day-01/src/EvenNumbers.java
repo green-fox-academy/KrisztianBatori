9172dabf369feac3da09b50ca400483857bc9789
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-07 12:32:46 +0100

    'Stream' - Java workshop --- FIXED empty Even numbers exercise

--- Week-06/Day-01/src/EvenNumbers.java
+++ Week-06/Day-01/src/EvenNumbers.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EvenNumbers {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14);
+        System.out.println(
+                numbers.stream().filter(number -> number % 2 == 0).collect(Collectors.toList())
+        );
+    }
 }
