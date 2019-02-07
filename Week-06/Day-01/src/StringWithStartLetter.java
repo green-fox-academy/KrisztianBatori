b543a86e739b3d4502420c9948af566a210f85d5
KrisztianBatori <batori.krisztian97@gmail.com>
2019-02-07 17:38:37 +0100

    'Exercise 7' - Java workshop (week 6) DONE

--- Week-06/Day-01/src/StringWithStartLetter.java
+++ Week-06/Day-01/src/StringWithStartLetter.java
@@ -1,17 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringWithStartLetter {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(stringsStartWith('R')));
+        System.out.println(Arrays.toString(stringsStartWith('A')));
     }
 
     public static String[] stringsStartWith(char character) {
         List<String> cities = Arrays.asList("ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS");
 
-        return cities.stream()
+        return (cities.stream()
                 .filter(string -> string.charAt(0) == character)
-                .toArray();
+                .collect(Collectors.toList())).toArray(String[]::new);
     }
 }
