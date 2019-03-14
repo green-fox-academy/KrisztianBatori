c4cecc9d4c3bdd792264f5df0fc3f84baed0f1fb
KrisztianBatori <batori.krisztian97@gmail.com>
2019-03-14 11:35:04 +0100

    There were still changes in these files so I decided to get rid of them and commit

---
+++ Week-04/src/aircraftcarrier/Aircraft.java
@@ -0,0 +1,44 @@
+public class Aircraft {
+    public final String type;
+    private int max_ammo;
+    public final int base_damage;
+    public int ammo_store = 0;
+    public boolean isPriority;
+
+    Aircraft(String type, int max_ammo, int base_damage) {
+        this.type = type;
+        this.max_ammo = max_ammo;
+        this.base_damage = base_damage;
+        if (type.equals("F16")) {
+            isPriority = false;
+        }
+        else {
+            isPriority = true;
+        }
+    }
+
+    public int fight() {
+        int damage = base_damage * ammo_store;
+        ammo_store = 0;
+        return damage;
+    }
+
+    public int refill(int ammo) {
+        if (ammo > max_ammo) {
+            ammo_store = max_ammo;
+            return ammo - max_ammo;
+        }
+        else if (ammo > 0) {
+            ammo_store += ammo;
+        }
+        return 0;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void getStatus() {
+        System.out.println("Type " + type + ", Ammo: " + ammo_store + ", Base Damage: " + base_damage + ", All Damage: " + ammo_store * base_damage);
+    }
+}
---
+++ Week-04/src/aircraftcarrier/Carrier.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Carrier {
+    public ArrayList<Aircraft> aircrafts;
+    private int ammoStorage;
+    public int healthPoints;
+
+    Carrier(ArrayList<Aircraft> aircrafts, int ammoStorage, int healthPoints) {
+        this.aircrafts = aircrafts;
+        this.ammoStorage = ammoStorage;
+        this.healthPoints = healthPoints;
+    }
+
+    public void add(Aircraft aircraft) {
+        aircrafts.add(aircraft);
+    }
+
+    public void fill() {
+        if (ammoStorage < 0) {
+            throw new IllegalArgumentException("The aircraft has no ammo.");
+        }
+
+        if (ammoStorage < 1000) {
+            aircrafts = (ArrayList<Aircraft>)aircrafts.stream()
+                    .map(aircraft -> {
+                        if (aircraft.isPriority) {
+                            ammoStorage = aircraft.refill(ammoStorage);
+                        }
+                        return aircraft;
+                    })
+                    .collect(Collectors.toList());
+            aircrafts = (ArrayList<Aircraft>)aircrafts.stream()
+                    .map(aircraft -> {
+                        if (!aircraft.isPriority) {
+                            ammoStorage = aircraft.refill(ammoStorage);
+                        }
+                        return aircraft;
+                    })
+                    .collect(Collectors.toList());
+        }
+        else {
+            for (int i = 0; i < aircrafts.size(); i++) {
+                ammoStorage = aircrafts.get(i).refill(ammoStorage);
+            }
+        }
+    }
+
+    public void fight(Carrier enemyCarrier) {
+        while (ammoStorage != 0) {
+            // Attacking enemy carrier.
+            enemyCarrier.healthPoints -= getTotalDamage();
+            if (enemyCarrier.healthPoints <= 0) {
+                break;
+            }
+            aircrafts = (ArrayList<Aircraft>)aircrafts.stream()
+                    .map(aircraft -> {
+                        aircraft.fight();
+                        return aircraft;
+                    })
+                    .collect(Collectors.toList());
+            fill();
+
+            // Enemy carrier counter-attacking.
+            healthPoints -= enemyCarrier.getTotalDamage();
+            if (healthPoints <= 0) {
+                break;
+            }
+            enemyCarrier.aircrafts = (ArrayList<Aircraft>)enemyCarrier.aircrafts.stream()
+                    .map(aircraft -> {
+                        aircraft.fight();
+                        return aircraft;
+                    })
+                    .collect(Collectors.toList());
+            enemyCarrier.fill();
+        }
+    }
+
+    public int getTotalDamage() {
+        return Arrays.stream(
+                aircrafts.stream()
+                        .map(aircraft -> aircraft.base_damage * aircraft.ammo_store)
+                        .mapToInt(i -> i)
+                        .toArray()
+        ).sum();
+    }
+
+    public void getStatus() {
+        if (healthPoints <= 0) {
+            System.out.println("It's dead Jim :(");
+        }
+        else {
+            System.out.println("HP: " + healthPoints + ", Aircraft count: " + aircrafts.size() + ", Ammo Storage: " + ammoStorage + ", Total damage: " + getTotalDamage());
+            System.out.println("Aircrafts:");
+            for (Aircraft aircraft: aircrafts) {
+                System.out.println("Type: " + aircraft.type + ", Ammo: " + aircraft.ammo_store + ", Base Damage: " + aircraft.base_damage + ", All Damage: " + aircraft.fight());
+            }
+        }
+    }
+}
