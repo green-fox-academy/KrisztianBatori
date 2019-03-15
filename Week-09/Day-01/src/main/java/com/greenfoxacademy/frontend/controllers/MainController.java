bc7f508ac010f441e456db3d4aea60c8ddd842b4
KrisztianBatori <batori.krisztian97@gmail.com>
2019-03-15 16:29:48 +0100

    DONE doubling exercise and using REST controller now

--- Week-09/Day-01/src/main/java/com/greenfoxacademy/frontend/controllers/MainController.java
+++ Week-09/Day-01/src/main/java/com/greenfoxacademy/frontend/controllers/MainController.java
@@ -1,14 +1,38 @@
 package com.greenfoxacademy.frontend.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.greenfoxacademy.frontend.models.Doubling;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+import java.util.OptionalInt;
+
+@RestController
 public class MainController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @RequestMapping("/doubling")
+    public ObjectNode getDouble(@RequestParam(name = "input", required = false) Integer input) {
+        if (input == null) {
+            ObjectNode objectNode = new ObjectMapper().createObjectNode();
+            objectNode.put("error", "Please provide an input!");
+            return objectNode;
+        }
+        else {
+            return new ObjectMapper().valueToTree(
+                    new Doubling(input, input * 2)
+            );
+        }
     }
 }
---
+++ Week-09/Day-01/src/main/java/com/greenfoxacademy/frontend/models/Doubling.java
@@ -0,0 +1,35 @@
+package com.greenfoxacademy.frontend.models;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Doubling {
+    private int received;
+    private int result;
+
+    public Doubling() {
+
+    }
+
+    public Doubling(int received, int result) {
+        this.received = received;
+        this.result = result;
+    }
+
+    public int getReceived() {
+        return received;
+    }
+
+    public void setReceived(int received) {
+        this.received = received;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
